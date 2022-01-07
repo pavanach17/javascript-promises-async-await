292b18ab6d41840df438e66204f16b86761aeda8
pavanach17 <96292549+pavanach17@users.noreply.github.com>
2022-01-07 10:08:12 +0530

    Module-3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-export function fetchMovies() {}
+import {fetchWithTimeout} from "./services";
+
+const aweFunky = require("./data/movies.json");
+export function fetchMovies() {
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
+}
+
+const moviePromise = fetchMovies();
+moviePromise.then(results => {
+    console.log(results)
+    });
