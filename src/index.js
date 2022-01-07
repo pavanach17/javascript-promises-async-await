35f83028fa5a2edf01a6d22597fd65704753e5da
pavanach17 <96292549+pavanach17@users.noreply.github.com>
2022-01-07 12:03:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,17 +38,13 @@ async function getBooksAndMoviesAsync(){
     }
     catch(error){
         console.log("Error fetching books and movies", error);
+        return error;
     }
 }
 
 async function getBooksOrMoviesAsync(){
-    try{
-        const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
-        return values;
-    }
-    catch(error){
-        console.error("Error waiting for the promise race", error);
-    }
+    const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
+    return values;
 }
 
 getBooksAndMoviesAsync()
@@ -57,6 +53,8 @@ getBooksAndMoviesAsync()
             movies: results.movies,
             books: results.books
           });
+    }).catch(error => {
+        console.error("Error in getBooksAndMoviesAsync execution", error);
     });
 
 getBooksOrMoviesAsync().then(results => {
